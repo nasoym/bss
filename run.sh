4b04aedc9b35999928eb6c8e413a3ab145725762
Sinan Goo <sinan.goo@hypoport.de>
2017-03-27 14:17:12 +0200

    simplify options

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -ef -o pipefail
 
-while getopts "p:v" options; do case $options in
-  v) SOCAT_OPTIONS="-vv" ;;
+while getopts "p:" options; do case $options in
   p) PORT="$OPTARG" ;;
 esac; done; shift $(( OPTIND - 1 ))
 
 : ${PORT:="8080"}
-: ${SOCAT_OPTIONS:=""}
+: ${SOCAT_OPTIONS:=""} #-vv
 : ${SERVICE:="$(dirname $0)/service.sh"}
 
 socat_listen_command="TCP-LISTEN:${PORT},reuseaddr,fork"
