16d3bbbfd55cd440cf76eeb776ed611abdb9d854
Sinan Goo <sinan.goo@hypoport.de>
2017-03-27 14:20:55 +0200

    add authentication and use more helper functions

--- service.sh
+++ service.sh
@@ -6,44 +6,53 @@ source $(dirname $0)/lib/logger
 source $(dirname $0)/lib/http_helpers
 source $(dirname $0)/lib/parse_request
 source $(dirname $0)/lib/find_handler_file
+source $(dirname $0)/lib/jwt_verify
 
 : ${ROUTES_PATH:="$(dirname $0)/handlers"}
 : ${DEFAULT_ROUTE_HANDLER:="${ROUTES_PATH}/default"}
+: ${AUTHENTICATE:="1"}
+: ${VERBOSE_LOGGING:="0"}
+export VERBOSE_LOGGING="$VERBOSE_LOGGING"
 
 parse_request
 log "request: ${SOCAT_PEERADDR}:${SOCAT_PEERPORT} ${request_method} ${request_uri}"
+
+if [[ "$AUTHENTICATE" = 1 ]];then
+  if [[ -z "$request_header_authorization" ]];then
+    authorization_token="$( echo "$request_header_cookie" \
+      | sed -e 's/; */\n/g' \
+      | awk -F '=' '{if ($1=="authentication") {print $2}}' )"
+  else
+    authorization_token="${request_header_authorization#* }"
+  fi
+  public_key_file="public_keys/public_key"
+  if ! jwt_verify "$authorization_token" $public_key_file; then
+    log "jwt signature failed"
+    echo_response_status_line 401 "Unauthorized"
+    echo_response_default_headers
+    echo -e "\r"
+    exit 0
+  fi
+fi
+
 find_handler_file $request_path
 
 if [[ -n "$request_matching_route_file" ]];then
-  # RESPONSE_CONTENT="$(echo "$request_content" | $request_matching_route_file "${COMMAND_ARGUMENTS[@]}" $(urldecode ${request_subpath//\// }))"
   RESPONSE_CONTENT="$(echo "$request_content" | $request_matching_route_file $(urldecode ${request_subpath//\// }))"
   if [[ $? -eq 1 ]];then
     echo_response_status_line 500 "Internal Server Error"
     echo_response_default_headers
     echo -e "\r"
-    exit 0
-  fi
-  if [[ "$RESPONSE_CONTENT" =~ ^HTTP\/[0-9]+\.[0-9]+\ [0-9]+ ]];then
-    echo "${RESPONSE_CONTENT}"
   else
-    echo_response_status_line  
-    echo_response_default_headers
-    echo -e "Content-Type: text/html\r"
-    echo -e "Content-Length: ${#RESPONSE_CONTENT}\r"
-    echo -e "\r"
-    echo "${RESPONSE_CONTENT}"
+    if [[ "$RESPONSE_CONTENT" =~ ^HTTP\/[0-9]+\.[0-9]+\ [0-9]+ ]];then
+      echo "${RESPONSE_CONTENT}"
+    else
+      echo "${RESPONSE_CONTENT}" | echo_content_response
+    fi
   fi
 else
   echo_response_status_line 404 "Not Found"
   echo_response_default_headers
   echo -e "\r"
 fi
-
-
-
-# printenv | grep -i "request" >&2
-# printenv | grep -i "socat" >&2
-# printenv >&2
-
-# echo "foobar"
 
